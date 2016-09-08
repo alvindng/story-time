871eb17408592596495183a43fcb400a5596420e
Alvin Ng and Leah Nelson <alvin@>
2016-09-08 09:55:25 -0700

    add show view for individual stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,4 +2,26 @@ class StoriesController < ApplicationController
   def index
     @stories = Story.all
   end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to stories_path
+    else
+      render :new
+    end
+  end
+
+private
+  def story_params
+    params.require(:story).permit(:name, :image)
+  end
 end
